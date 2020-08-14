16f6801c0ddddcc871f3ed531c71cad2aad31d17
Roge Rodrigues <roge17r@gmail.com>
2020-08-13 23:15:04 -0300

    finished hidden info app

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import QRCode from 'react-native-qrcode-svg';
-
-import {Container, Code} from './styles';
+import {
+  Container,
+  Code,
+  QRCode,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
 
 const Menu: React.FC = () => {
   return (
     <>
       <Container>
         <Code>
-          {/* <QRCode
-            value="https://github.com/roge17r"
-            size={80}
-            backgroundColor="#fff"
-            color="#8b10ae"
-          /> */}
+          <QRCode />
         </Code>
+
+        <Nav>
+          <NavItem>
+            <Icon name="help-outline" size={20} color="#fff" />
+            <NavText>Me ajuda</NavText>
+          </NavItem>
+          <NavItem>
+            <Icon name="person-outline" size={20} color="#fff" />
+            <NavText>Perfil</NavText>
+          </NavItem>
+          <NavItem>
+            <Icon name="credit-card" size={20} color="#fff" />
+            <NavText>Configurar cartão</NavText>
+          </NavItem>
+          <NavItem>
+            <Icon name="smartphone" size={20} color="#fff" />
+            <NavText>Configurações do app</NavText>
+          </NavItem>
+        </Nav>
+
+        <SignOutButton onPress={() => {}}>
+          <SignOutButtonText>Sair do app</SignOutButtonText>
+        </SignOutButton>
       </Container>
     </>
   );
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,5 +1,53 @@
 import styled from 'styled-components/native';
+import {StyleSheet} from 'react-native';
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView`
+  margin: 0 30px;
+`;
 
-export const Code = styled.View``;
+export const Code = styled.View`
+  background: #dddddd;
+  padding: 10px;
+  align-self: center;
+`;
+
+export const QRCode = styled.View`
+  width: 80px;
+  height: 80px;
+  background: #ffffff;
+`;
+
+export const Nav = styled.View`
+  margin-top: 30px;
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-top-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 0;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: rgba(255, 255, 255, 0.8);
+`;
+
+export const NavText = styled.Text`
+  font-size: 15px;
+  color: #ffffff;
+  margin-left: 20px;
+`;
+
+export const SignOutButton = styled.TouchableOpacity`
+  border-color: rgba(255, 255, 255, 0.8);
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  margin-top: 15px;
+`;
+
+export const SignOutButtonText = styled.Text`
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 13px;
+`;
